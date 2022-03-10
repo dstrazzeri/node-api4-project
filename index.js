5fd967cc4ceb9c8a478a97ed6df3aa014ecb7b0a
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2022-03-10 13:12:05 -0500

    push it real good

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const server = express()
+const port = process.env.PORT || 9000
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/api/oh hi', (req, res) => {
+    res.json({ message: 'ITS ALIVE' })
+})
+
+server.use('*', (req, res) => {
+    res.send(`<h1>suuhhhhh duuuudeee</h1>`)
+})
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+server.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
